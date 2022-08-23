81bcd1c2b13fbd81bc6d11cd8b4bf11f1189ebb5
garryshield <749291704@qq.com>
2022-08-23 12:31:28 +0800

    init

--- user/t1/hook.sh
+++ user/t1/hook.sh
@@ -11,8 +11,26 @@ hook_target_b() {
 
     pwd
 
+    #### luci-app-openclash
     svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash openwrt/package/luci-app-openclash
-    svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome openwrt/package/luci-app-adguardhome
+
+    #### luci-app-adguardhome
+    # svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome openwrt/package/luci-app-adguardhome
+    git clone https://github.com/rufengsuixing/luci-app-adguardhome openwrt/package/luci-app-adguardhome
+    # resolv.conf.auto
+    # check_if_already_running
+
+    #### luci-app-onliner
+    git clone https://github.com/rufengsuixing/luci-app-onliner openwrt/package/luci-app-onliner
+
+    #### luci-app-advanced
+    git clone https://github.com/sirpdboy/luci-app-advanced openwrt/package/luci-app-advanced
+
+    #### luci-app-filebrowser
+    git clone https://github.com/giaulo/luci-app-filebrowser openwrt/package/luci-app-filebrowser
+
+    #### luci-app-poweroff
+    git clone https://github.com/esirplayground/luci-app-poweroff openwrt/package/luci-app-poweroff
 
     cp user/t1/.config openwrt/.config
 
