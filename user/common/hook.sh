834d373e13d86a97d5d74977e7fc4c5fc8ed3cba
garryshield <749291704@qq.com>
2022-08-25 18:43:54 +0800

    init

--- user/common/hook.sh
+++ user/common/hook.sh
@@ -7,7 +7,57 @@ hook_common_a() {
 }
 
 hook_common_b() {
-    echo 'hook_common_b'
+    echo 'hook_common_b start'
+
+    pwd
+
+    #### default-settings
+    cat <<EOF >openwrt/package/base-files/files/etc/uci-defaults/9999-default-settings
+####################################
+# /etc/config/network
+####
+
+# lan
+uci set network.lan.proto='static'
+uci set network.lan.ipaddr='10.10.1.1'
+uci set network.lan.netmask='255.255.255.0'
+
+# wan
+if [ -n "\$(uci -q get network.wan)" ]; then
+    uci set network.wan.proto='pppoe'
+    uci set network.wan.username=''
+    uci set network.wan.password=''
+fi
+
+uci commit network
+
+####################################
+# /etc/openwrt_release
+# /etc/banner
+####
+TMP="\$(cat /etc/openwrt_release | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/\${TMP}/\${TMP} (\$(date +"%Y-%m-%d")) By GarryShield/g" /etc/openwrt_release
+
+cat <<EOF1 >/etc/banner
+     _________
+    /        /\      _    ___ ___  ___
+   /  LE    /  \    | |  | __|   \| __|
+  /    DE  /    \   | |__| _|| |) | _|
+ /________/  LE  \  |____|___|___/|___|
+ \        \   DE /
+  \    LE  \    /  -------------------------------------------
+   \  DE    \  /    %D %V, %C
+    \________\/    -------------------------------------------
+
+--------------------------------------------------------------
+${TMP} ($(date +"%Y-%m-%d")) By GarryShield
+--------------------------------------------------------------
+EOF1
+
+exit 0
+EOF
+
+    echo 'hook_common_b end'
 }
 
 hook_common_c() {
--- user/t2/hook.sh
+++ user/t2/hook.sh
@@ -27,51 +27,6 @@ hook_target_b() {
     #### luci-app-oaf
     git clone https://github.com/destan19/OpenAppFilter.git openwrt/package/t2/OpenAppFilter
 
-    cat <<EOF >openwrt/package/base-files/files/etc/uci-defaults/9999-default-settings
-####################################
-# /etc/config/network
-####
-
-# lan
-uci set network.lan.proto='static'
-uci set network.lan.ipaddr='10.10.1.1'
-uci set network.lan.netmask='255.255.255.0'
-
-# wan
-if [ -n "\$(uci -q get network.wan)" ]; then
-    uci set network.wan.proto='pppoe'
-    uci set network.wan.username=''
-    uci set network.wan.password=''
-fi
-
-uci commit network
-
-####################################
-# /etc/openwrt_release
-# /etc/banner
-####
-TMP="\$(cat /etc/openwrt_release | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
-sed -i "s/\${TMP}/\${TMP} (\$(date +"%Y-%m-%d")) By GarryShield/g" /etc/openwrt_release
-
-cat <<EOF1 >/etc/banner
-     _________
-    /        /\      _    ___ ___  ___
-   /  LE    /  \    | |  | __|   \| __|
-  /    DE  /    \   | |__| _|| |) | _|
- /________/  LE  \  |____|___|___/|___|
- \        \   DE /
-  \    LE  \    /  -------------------------------------------
-   \  DE    \  /    %D %V, %C
-    \________\/    -------------------------------------------
-
---------------------------------------------------------------
-${TMP} ($(date +"%Y-%m-%d")) By GarryShield
---------------------------------------------------------------
-EOF1
-
-exit 0
-EOF
-
     cp user/t2/.config openwrt/.config
 
     echo 'hook_target_b end'
